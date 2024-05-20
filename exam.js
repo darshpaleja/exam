b96b733d0e3680aed16eee5a1bf7e61c9ef6e56a
darshpaleja <163432025+darshpaleja@users.noreply.github.com>
2024-05-20 08:45:09 +0530

    exam.js

--- exam.js
+++ exam.js
@@ -1,40 +1,24 @@
 let form = document.getElementById('form')
 
-let inputBox = document.getElementById('input')
 let submit = document.getElementById('submitBtn')
-// let ul = document.getElementById('ul')
-// let li = document.getElementById('li')
 
-let items = []
+let ul = document.getElementById('ul')
 
-// console.log(ul);
-// console.log(li);
+form.addEventListener('submit', createListItem)
 
-// form.addEventListener('submit', createListItem)
+function createListItem(e) {
 
-function inputClick() {
-    var inputValue = inputBox.value
-    // console.log(inputValue);
-    return inputValue
+    e.preventDefault()
+    let inputBox = document.getElementById('input').value
+    
+    ul.className = 'el'
+
+    let li = document.createElement('li')
+    li.id = 'list'
+    li.innerText = inputBox
+
+    ul.appendChild(li)
 }
-
-function submitClick() {
-    let value = inputClick()
-    // console.log(value);
-    items.push(value) 
-    createListItem()
-    // console.log(items)
-}
-
-function createListItem() {
-    let ul = document.querySelector('ul')
-    items.forEach((item) => {
-        var li = document.createElement('li')
-        ul.appendChild(li)
-        li.innerText += item
-    })
-}
-
 
 
 
